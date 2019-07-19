e6dfff616c91439ba0619b4c7f861c61f1872819
pankhuri-52 <pankhuritrikha@gmail.com>
2019-07-19 20:01:59 +0530

    work with macros

---
+++ macros.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#define multiply(a,b) a*b
+#define concat(a,b) a##b
+#define coutmine(n)\
+	int i=0;\
+	while(i<n)\
+         {\
+		printf("%d\n",i);\
+		 i++;\
+	}
+int main(void){
+	int c=multiply(2+3,4+5);
+	printf("%d\n",c);
+	printf("%d\n",concat(12,08));
+	int a;
+	a=10;
+	printf("%p",&a);
+	coutmine(5);
+	return 0;
+}
