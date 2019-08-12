7b2f6a8dd99556e7cf8ae8cce7172779544d4116
pankhuri-52 <pankhuritrikha@gmail.com>
2019-08-12 10:19:05 +0530

    90 degree rotation of 2d array

--- 90degrotate.cpp
+++ 90degrotate.cpp
@@ -5,25 +5,21 @@ int main(){
 	cout<<"Enter the size of array"<<endl;
 	cin>>n;
 	int arr[n][n];
+	int temp[n][n];
+	int k=0;
 	cout<<"Enter the elements"<<endl;
 	for(int i=0;i<n;i++){
 		for(int j=0;j<n;j++){
 			cin>>arr[i][j];
 		}
 	}
-	cout<<"2darray after transposing"<<endl;
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-			cout<<arr[j][i]<<" ";
-		}
-		cout<<endl;
-	}
-	cout<<"2darray after rotation it 90degree"<<endl;
+	cout<<"2darray after mirror imaging"<<endl;
 	for(int i=0;i<n;i++){
 		for(int j=n-1;j>=0;j--){
-			cout<<arr[j][i]<<" ";
+			cout<<arr[i][j]<<" ";
 		}
 		cout<<endl;
 	}
+
 	return 0;
 }
