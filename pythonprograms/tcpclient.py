1e76dcd1497451844bcac10840f23ba96c2d0025
pankhuri-52 <49311757+pankhuri-52@users.noreply.github.com>
2019-10-03 23:08:26 +0530

    Add files via upload
    This is a file which receives message from server in python

--- pythonprograms/tcpclient.py
+++ pythonprograms/tcpclient.py
@@ -1,15 +1,8 @@
-"""
-TCP client code
-client wants to connect to server
-"""
-
 import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-
-#connecting with thw server
 s.connect(('127.0.0.1',12345))
-
-#printing the message that the server sent
-print(s.recv(2048))
+msg=s.recv(2048)
+print(msg.decode())
+if msg.decode()=="HELLO":
+    s.send(b"HII")
 s.close()
-
