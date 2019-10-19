0c7bad1d070f975985a295a2baa5155fdc1e29e7
pankhuri-52 <pankhuritrikha@gmail.com>
2019-10-19 23:24:29 +0530

    Removing the duplicates which are occuring more than thrice

--- Stacks/reduce_the_string.cpp
+++ Stacks/reduce_the_string.cpp
@@ -3,33 +3,45 @@
 #include <stack>
 using namespace std;
 void remove_duplicates(string str){
-	stack <char> stk;
-	int n=str.length();
-	char char_array[n+1];
-	int count=0;
-	strcpy(char_array,str.c_str());
-	for(int i=0;i<n;i++){
-		if(char_array[i]!=char_array[i+1]){
-			stk.push(char_array[i]);
-			 if(count>=4){
-                        while(count!=0){
-                                stk.pop();
-				count--;
-                        }
+        stack <char> stk;
+        int n=str.length();
+        char char_array[n+1];
+        int count=0;
+        strcpy(char_array,str.c_str());
+stk.push(char_array[0]);
+        for(int i=1;i<=n;i++){
+                if(char_array[i]!=stk.top())
+           {
+             if(count>=2)
+             {
+               while(count!=0)
+                 {
+                        stk.pop();
+                        count--;
+                  }
+                 stk.pop();
+             }
+            stk.push(char_array[i]);
+             count=0;
+            }
+                else {
+                        count++;
+                        stk.push(char_array[i]);
                 }
-           }
-		if(char_array[i]==char_array[i+1]){
-			count++;
-			stk.push(char_array[i]);
-		}
-	}
-	while(stk.empty()==false){
-		cout<<stk.top();
-		stk.pop();
+        }
+	stack <char> stk1;
+        while(stk.empty()==false){
+                stk1.push(stk.top());
+                stk.pop();
+        }
+	while(stk1.empty()==false){
+		cout<<stk1.top();
+		stk1.pop();
 	}
 }
 int main(int argc,const char *argv[]){
-	string str=argv[1];
-	remove_duplicates(str);
-	return 0;
+        string str=argv[1];
+        remove_duplicates(str);
+        return 0;
 }
+
